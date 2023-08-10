75414a562d0073ab44338b01dca41403a8e3e80e
oumaima <oumaimanaanaapro@gmail.com>
2023-08-10 06:27:57 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * this is a file that prints exacly
+ * \"Programming is like building a multilingual puzzle
+ */
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
