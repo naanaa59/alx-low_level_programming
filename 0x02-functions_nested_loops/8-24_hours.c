a65c967578721fcfcbd21abf3f3b2da85c76a5e7
oumaima <oumaimanaanaapro@gmail.com>
2023-08-15 18:14:36 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,46 +5,31 @@
  */
 void jack_bauer(void)
 {
-	int d4 = 0;
+	int dd = 0;
 
-	while (d4 <= 2)
+	while (dd <= 23)
 	{
-		int d3 = 0;
+		int d4 = dd / 10;
+		int d3 = dd % 10;
+		int d2 = 0;
 
-		while (d3 <= 9)
+		while (d2 <= 5)
 		{
-			int d2 = 0;
+			int d1 = 0;
 
-			while (d2 <= 5)
+			while (d1 <= 9)
 			{
-				int d1 = 0;
-
-				while (d1 <= 9)
-				{
-					if (d1 ==2  && d2 < 4 && d3 == 0 && d4 == 0)
-					{
-						_putchar('\n');
-						_putchar('\n');
-						_putchar('\n');
-						_putchar('\n');
-						_putchar('\n');
-						_putchar('\n');
-					}
-					else
-					{
-					_putchar(d4 + '0');
-					_putchar(d3 + '0');
-					_putchar(':');
-					_putchar(d2 + '0');
-					_putchar(d1 + '0');
-					_putchar('\n');
-					}
-					d1++;
-				}
-				d2++;
+				_putchar(d4 + '0');
+				_putchar(d3 + '0');
+				_putchar(':');
+				_putchar(d2 + '0');
+				_putchar(d1 + '0');
+				_putchar('\n');
+				d1++;
 			}
-			d3++;
+			d2++;
 		}
-		d4++;
+		dd++;
 	}
 }
+
