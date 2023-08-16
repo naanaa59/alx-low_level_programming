09ba46cdce9bc256d580fc429255407666cd57b5
oumaima <oumaimanaanaapro@gmail.com>
2023-08-16 08:05:17 -0700

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *sum_of_multiples - returns sum of multiples of 3 and 5 belox 1024 excluded
+ *
+*/
+void sum_of_multiples(void)
+{
+	int i = 0, sum;
+
+	while (i >= 0)
+	{
+		sum = (3 * i) + (5 * i);
+		if (sum > 1024)
+			break;
+	i++;
+	}
+	printf("%d\n", sum);
+}
