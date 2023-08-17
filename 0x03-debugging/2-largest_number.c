1d2fe533c93d4366baedd7544af70eaa093c3d9c
oumaima <oumaimanaanaapro@gmail.com>
2023-08-17 09:38:41 -0700

    change the if statments

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && b > c a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && a > c && b > c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else if (c > a && a > b && c > b)
 	{
 		largest = c;
 	}
