908eccdab76c50e927a29d8b4b2429f253fe3f76
oumaima <oumaimanaanaapro@gmail.com>
2023-08-17 10:51:58 -0700

    task7 corrected with if stat

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,8 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int i; 
+	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
-
+		for (j = 1; j <= n; j++)
+		{
+			if (i == j)
+				break;
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
