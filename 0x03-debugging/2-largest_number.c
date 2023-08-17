c35c92e31733c3b525c8a3a00df1cfec80bfa883
oumaima <oumaimanaanaapro@gmail.com>
2023-08-17 10:36:03 -0700

    add equals

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,29 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (a > b && b == c)
-		largest = a;
-	else if (a > c && b == c)
-		largest = a;
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (b > a  && a == c)
-		largest = b;
-	else if (b > c && a == c)
-		largest = b;
-	else if (c > a && a > b)
+	else if (c >= a  && c >= b)
 	{
 		largest = c;
 	}
-	else if (c > a && b == c)
-		largest = c;
-	else if (c > b && b == c)
-		largest = c;
 	return (largest);
 }
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int positive_or_negative(int n)
+{
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
