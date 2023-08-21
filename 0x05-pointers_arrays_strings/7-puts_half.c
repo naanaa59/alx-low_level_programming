60b447ea61aeb95fecda4fba847485c3aaffc31e
oumaima <oumaimanaanaapro@gmail.com>
2023-08-21 13:08:53 -0700

    first commit of task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: given string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s);
+int _strlen(char *s)
+{
+	int l = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		l++;
+	}
+	return (l);
+}
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ *
+ * @str: the given string
+ */
+void puts_half(char *str)
+{
+	int i, l;
+
+	l = _strlen(str);
+	for (i = l / 2; i < l; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
