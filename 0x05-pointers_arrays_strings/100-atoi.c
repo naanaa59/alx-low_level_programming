7b0b49fdd6ad6b2713b554d80ad8dd64380786ee
oumaima <oumaimanaanaapro@gmail.com>
2023-08-21 16:55:43 -0700

    first commit of task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ *
+ * @s: the string to convert
+ *
+ * Return: int value of the string
+ */
+int _atoi(char *s)
+{
+	int i, num = 0, sign = 1;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+
+	}
+	return (num * sign);
+}
