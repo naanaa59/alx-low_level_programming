24009a8c006fac31895fed73cccadddd45e88889
oumaima <oumaimanaanaapro@gmail.com>
2023-08-21 12:47:50 -0700

    add if statement to correct output

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != 0)
+	_putchar(str[0]);
+	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
 		_putchar(str[i]);
-		i = i + 2;
+		i++;
 	}
 	_putchar('\n');
 }
