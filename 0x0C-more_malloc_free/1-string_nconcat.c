081e59085331c7a44191d37a2302ea40fe67192c
oumaima <oumaimanaanaapro@gmail.com>
2023-09-07 15:21:02 -0700

    task1 with empty strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * string_nconcat - concantenates two string
  *
@@ -14,14 +15,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j;
+	unsigned int i = 0, j;
 
-	s = malloc(sizeof(s1) + n + 1);
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL)
 	{
-		s[i] = s1[i];
+		s = malloc(n + 1);
+		if (s == NULL)
+			return (NULL);
+		s[0] = '\0';
+	}
+	else
+	{
+		s = malloc(strlen(s1) + n + 1);
+		if (s == NULL)
+			return (NULL);
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			s[i] = s1[i];
+		}
+	}
+	if (s2 == NULL)
+	{
+		s[i] = '\0';
+		return (s);
 	}
 	for (j = 0; j < n; j++)
 	{
