78660a0fd6566d36c6612f3f4d8ffe9a08ac77bc
oumaima <oumaimanaanaapro@gmail.com>
2023-09-11 15:00:45 -0700

    (nil)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == 0 || d->age == 0 || d->owner == 0)
-		printf("nil");
+		printf("(nil)");
 	else
 	{
 		printf("Name: %s\n", d->name);
