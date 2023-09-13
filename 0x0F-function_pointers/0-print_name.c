da5b56d555408fe5454f77178a2a40fc3b0dba1a
oumaima <oumaimanaanaapro@gmail.com>
2023-09-13 06:14:56 -0700

    task0 header guarded and empty name case added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || name == '\0')
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -9,3 +13,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
