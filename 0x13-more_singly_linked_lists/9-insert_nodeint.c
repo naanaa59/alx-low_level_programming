def644ad0e79b52761082a8734ed69564f6ae934
oumaima <oumaimanaanaapro@gmail.com>
2023-09-26 12:20:41 -0700

    handling if index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if ((*head)->next == NULL)
+	if (idx == 0)
 	{
 		(*head)->next = new;
 		return (new);
