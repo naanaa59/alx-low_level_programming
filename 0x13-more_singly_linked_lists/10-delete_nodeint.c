5ba7b935261b42341157575498edbb4f0c3fa816
oumaima <oumaimanaanaapro@gmail.com>
2023-09-26 13:39:23 -0700

    handle return value

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	int flag;
+	int flag = 1;
 	listint_t *current, *tmp;
 
 	if (*head == NULL)
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
-		flag = -1;
+		flag = 1;
 	}
 	else
 	{
@@ -41,6 +41,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			current->next = tmp->next;
 			free(tmp);
 			flag = 1;
+		}
+		else
+		{
+			flag = -1;
 		}
 	}
 		return (flag);
