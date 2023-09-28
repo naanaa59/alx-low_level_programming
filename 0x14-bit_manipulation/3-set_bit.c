930b57993167953f169edb4fc9591e6902f763fb
oumaima <oumaimanaanaapro@gmail.com>
2023-09-28 11:02:05 -0700

    handling errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,10 +14,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (index >= sizeof(*n * 8))
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	mask = 1 << index;
+	mask = 1UL << index;
 	*n = *n | mask;
 	return (1);
 }
