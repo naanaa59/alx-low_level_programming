b6291c3c9f2b61f694d7f10ca721113ec5ebffb8
oumaima <oumaimanaanaapro@gmail.com>
2023-10-02 16:18:28 -0700

    task3 updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,74 +1,54 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_READ "Error: Can't read from file %s\n"
+#define ERR_WRITE "Error: Can't write to %s\n"
+#define ERR_CLOSE "Error: Can't close fd %d\n"
+#define RWRWR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define BUFFER_size 1024
+
 /**
- * cp_file - copies the content of a file into another
+ * main - entry point
+ * @argc: the number of arguments
+ * @argv: an array of arguments
  *
- * @file_from: the source file
- *
- * @file_to: the destination file
- *
+ * Return: Always 0 (success)
  */
 
-
-void cp_file(const char *file_from, const char *file_to)
+int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
-	ssize_t s_read, s_write;
-	char buffer[BUFFER_SIZE];
+	char *src, *dst, BUF[BUFFER_size];
+	int fdsrc, fddst;
+	ssize_t rd;
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		close(fd_from);
-		exit(99);
-	}
-	while ((s_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		s_write = write(fd_to, buffer, s_read);
-		if (s_write == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-		if (s_read == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			close(fd_from);
-			close(fd_to);
-			exit(100);
-		}
-		dprintf(2, "Successfully copied content from %s to %s\n",
-				file_from, file_to);
-	}
-}
-/**
- * main - Entry point
- *
- * @argc: argument count
- *
- * @argv: argument vector
- *
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
-	}
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 
-	cp_file(argv[1], argv[2]);
+	src = argv[1], dst = argv[2];
+
+	fdsrc = open(src, O_RDONLY);
+	if (fdsrc == -1)
+		dprintf(STDERR_FILENO, ERR_READ, src), exit(98);
+
+	fddst = open(dst, O_CREAT | O_WRONLY | O_TRUNC, RWRWR);
+	if (fddst == -1)
+		dprintf(STDERR_FILENO, ERR_WRITE, dst), exit(99);
+
+	while ((rd = read(fdsrc, BUF, BUFFER_size)) > 0)
+		if (write(fddst, BUF, rd) != rd)
+			dprintf(STDERR_FILENO, ERR_WRITE, dst), exit(99);
+
+	if (rd == -1)
+		dprintf(STDERR_FILENO, ERR_READ, src), exit(98);
+
+
+	if (close(fdsrc) == -1)
+		dprintf(STDERR_FILENO, ERR_CLOSE, fdsrc), exit(100);
+	if (close(fddst) == -1)
+		dprintf(STDERR_FILENO, ERR_CLOSE, fddst), exit(100);
+
 	return (0);
 }
