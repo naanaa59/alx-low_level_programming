aaba2845398b7302055807e91a393176bafdae9f
oumaima <oumaimanaanaapro@gmail.com>
2023-11-09 11:58:06 +0100

    task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees the doubly linked list
+ *
+ * @head: the head
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
