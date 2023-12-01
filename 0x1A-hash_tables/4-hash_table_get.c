f57a3cda3b858775a057eabbdc7817520f699538
oumaima <oumaimanaanaapro@gmail.com>
2023-12-01 16:59:46 +0100

    task3

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: the hash table
+ * @key: the key we re looking for
+ *
+ * Return: the value associated with the key or NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(ht->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+
+	return (NULL);
+}
