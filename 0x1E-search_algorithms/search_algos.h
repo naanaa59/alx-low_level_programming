6b1dc59ee3084e2440aa29bbac14bd3f58734f86
oumaima <oumaimanaanaapro@gmail.com>
2024-03-12 06:17:57 +0000

    betty header fixed

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,37 +11,34 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
+* struct listint_s - singly linked list
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next nodeDescription: singly linked list node structure
+*/
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- */
+* struct skiplist_s - Singly linked list with an express lane
+*
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+* @express: Pointer to the next node in the express lane
+*
+* Description: singly linked list node structure with an express lane
+*/
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
